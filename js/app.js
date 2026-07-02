b99b718c5347e71b61aba28a453dd611b74035ec
dorregoagus <dorregoagus18@gmail.com>
2026-07-02 15:09:31 -0300

    obtener y mostrar los detalles de los personajes

--- js/app.js
+++ js/app.js
@@ -15,16 +15,50 @@ const loading = document.getElementById("loading");
 const modalDetalleBody = document.getElementById("modalDetalleBody");
 // modal para bootstrap
 const modalDetalle = new bootstrap.Modal(document.getElementById("modalDetalle")),
-// fetch del listado de personajes
-async function obtenerListadoPersonajes() {
+    // fetch del listado de personajes
+    async function obtenerListadoPersonajes() {
+        try {
+            const respuesta = await fetch(URL_LISTADO);
+            if (!respuesta.ok) {
+                throw new Error("Error al obtener el listado de personajes");
+            }
+            const datos = await respuesta.json();
+            if (!datos.results || datos.results.length === 0) {
+                throw new Error("No se encontraron personajes");
+            }
+            personajes = datos.results;
+            renderizarTarjetas(personajes);
+        } catch (error) {
+            mostrarMensaje(
+                "No se pudo cargar la lista de personajes. Intenta recargar la p'agina.",
+                "danger"
+            );
+            console.error(error);
+        } finally {
+            loading.classList.add("d-none");
+        }
+    }
+// fetch del detalle de un personaje
+async function obtenerDetallePersonaje(id) {
     try {
-        const respuesta = await fetch(URL_LISTADO);
+        const respuesta = await fetch(`${URL_DETALLE}/${id}`);
+
         if (!respuesta.ok) {
-            throw new Error("Error al obtener el listado de personajes");
+            throw new Error("Error al obtener el detalle del personaje");
         }
-        const datos = await respuesta.json();
-        if (!datos.results || datos.results.length === 0) {
-            throw new Error("No se encontraron personajes");
+
+        const personaje = await respuesta.json();
+
+        if (!personaje || Object.keys(personaje).length === 0) {
+            throw new Error("La API no devolvió información del personaje");
         }
+
+        mostrarModalDetalle(personaje);
+    } catch (error) {
+        mostrarMensaje(
+            "No se pudo cargar el detalle de este personaje.",
+            "danger"
+        );
+        console.error(error);
     }
 }
